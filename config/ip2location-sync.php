e90120185337e46fbf55fd56a8f9c8146fd8dd3f
Eyüp Erdoğan <epsilonelittr@gmail.com>
2024-01-27 19:08:59 +0300

    Add ip2location token and database configs

--- config/ip2location-sync.php
+++ config/ip2location-sync.php
@@ -2,7 +2,36 @@
 
 return [
     /**
-     * Add any config description here.
+     * Ip2Location token
      */
-    'any_config' => true
+    'token' => env('IP2LOCATION_TOKEN'),
+
+    /**
+     * Ip type
+     * 
+     * values:
+     *  - IPV4
+     *  - IPV6
+     * 
+     * default: IPV6
+     */
+    'ip_type' => env('IP2LOCATION_IP_TYPE', 'IPV6'),
+
+    /**
+     * Database configuration
+     */
+    'database' => [
+        'driver' => 'mysql',
+        'host' => env('IP2LOCATION_MYSQL_HOST', '127.0.0.1'),
+        'port' => env('IP2LOCATION_MYSQL_PORT', '1010'),
+        'database' => env('IP2LOCATION_MYSQL_DBNAME', 'ip2location_database'),
+        'username' => env('IP2LOCATION_MYSQL_USERNAME', 'admin'),
+        'password' => env('IP2LOCATION_MYSQL_PASSWORD', 'secret'),
+        'unix_socket' => env('DB_SOCKET', ''),
+        'charset' => 'utf8mb4',
+        'collation' => 'utf8mb4_unicode_ci',
+        'prefix' => '',
+        'strict' => true,
+        'engine' => null,
+    ]
 ];
